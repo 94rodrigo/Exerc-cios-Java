8d7f4fad444b6179fba815b45f67b4006d58880a
94rodrigo <rodrigoc94@hotmail.com>
2020-04-18 15:07:59 -0300

    Commit - exercícios Java e exemplos - Curso Cod3r - Udemy

--- Exercícios java Completo - Curso Cod3r/src/colecoes/ConjuntoBaguncado.java
+++ Exercícios java Completo - Curso Cod3r/src/colecoes/ConjuntoBaguncado.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 public class ConjuntoBaguncado {
 
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static void main(String[] args) {
 		HashSet conjunto = new HashSet(); //não aceita repetição
 		
